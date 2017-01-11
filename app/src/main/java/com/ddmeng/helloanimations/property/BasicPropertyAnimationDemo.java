c40002ec8279807c91733ae608f8d5342a9f81f9
Dandan MENG <mengdandanno1@163.com>
2017-01-11 17:25:35 +0800

    add ValueAnimator demo

--- app/src/main/java/com/ddmeng/helloanimations/property/BasicPropertyAnimationDemo.java
+++ app/src/main/java/com/ddmeng/helloanimations/property/BasicPropertyAnimationDemo.java
@@ -1,15 +1,28 @@
 package com.ddmeng.helloanimations.property;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.ddmeng.helloanimations.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class BasicPropertyAnimationDemo extends AppCompatActivity {
+
+    @BindView(R.id.animation_view)
+    MyAnimationView myAnimationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_basic_property_animation_demo);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.play_button)
+    void onPlayButotnClick() {
+        myAnimationView.play();
     }
 }
---
+++ app/src/main/java/com/ddmeng/helloanimations/property/MyAnimationView.java
@@ -0,0 +1,103 @@
+package com.ddmeng.helloanimations.property;
+
+import android.animation.AnimatorSet;
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RadialGradient;
+import android.graphics.Shader;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.ddmeng.helloanimations.ShapeHolder;
+
+import java.util.ArrayList;
+
+public class MyAnimationView extends View {
+    private float density;
+    private final ArrayList<ShapeHolder> balls = new ArrayList<>();
+    private AnimatorSet animatorSet;
+
+    public MyAnimationView(Context context) {
+        super(context);
+        init();
+    }
+
+    public MyAnimationView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    private void init() {
+        density = getContext().getResources().getDisplayMetrics().density;
+        addBall(50f, 25f);
+        addBall(150f, 25f);
+        addBall(250f, 25f);
+        addBall(350f, 25f);
+        addBall(450f, 25f);
+    }
+
+    public void play() {
+        if (animatorSet == null) {
+            createAnimation();
+        }
+        animatorSet.start();
+    }
+
+    private void createAnimation() {
+        // Animation 1: Use ValueAnimator
+        final ShapeHolder ball1 = balls.get(0);
+        ValueAnimator animator1 = ValueAnimator.ofFloat(0f, getHeight() - ball1.getHeight());
+        animator1.setDuration(500);
+        animator1.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                ball1.setY((Float) animation.getAnimatedValue());
+                invalidate();
+            }
+        });
+
+        animatorSet = new AnimatorSet();
+        animatorSet.play(animator1);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        for (int i = 0; i < balls.size(); ++i) {
+            ShapeHolder shapeHolder = balls.get(i);
+            canvas.save();
+            canvas.translate(shapeHolder.getX(), shapeHolder.getY());
+            shapeHolder.getShape().draw(canvas);
+            canvas.restore();
+        }
+    }
+
+
+    private ShapeHolder addBall(float x, float y) {
+        OvalShape circle = new OvalShape();
+        circle.resize(50f * density, 50f * density);
+        ShapeDrawable drawable = new ShapeDrawable(circle);
+        ShapeHolder shapeHolder = new ShapeHolder(drawable);
+        shapeHolder.setX(x - 25f);
+        shapeHolder.setY(y - 25f);
+        int red = (int) (100 + Math.random() * 155);
+        int green = (int) (100 + Math.random() * 155);
+        int blue = (int) (100 + Math.random() * 155);
+        int color = 0xff000000 | red << 16 | green << 8 | blue;
+        Paint paint = drawable.getPaint(); // new
+        // Paint(Paint.ANTI_ALIAS_FLAG);
+        int darkColor = 0xff000000 | red / 4 << 16 | green / 4 << 8 | blue
+                / 4;
+        RadialGradient gradient = new RadialGradient(37.5f, 12.5f, 50f,
+                color, darkColor, Shader.TileMode.CLAMP);
+        paint.setShader(gradient);
+        shapeHolder.setPaint(paint);
+        balls.add(shapeHolder);
+        return shapeHolder;
+    }
+
+
+}
