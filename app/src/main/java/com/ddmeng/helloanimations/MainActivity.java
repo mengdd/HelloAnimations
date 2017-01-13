36330619aec2806fad13966e8a4b224d1cade6a0
Dandan MENG <mengdandanno1@163.com>
2017-01-13 18:16:51 +0800

    add simple fragment transition demo for v4 fragment

--- app/src/main/java/com/ddmeng/helloanimations/MainActivity.java
+++ app/src/main/java/com/ddmeng/helloanimations/MainActivity.java
@@ -17,6 +17,7 @@ import com.ddmeng.helloanimations.property.KeyframeDemoActivity;
 import com.ddmeng.helloanimations.property.ViewPropertyAnimationDemoActivity;
 import com.ddmeng.helloanimations.property.ZoomActivity;
 import com.ddmeng.helloanimations.transition.activity.SimpleActivityTransitionDemoActivity;
+import com.ddmeng.helloanimations.transition.fragment.SimpleFragmentTransitionDemoActivity;
 import com.ddmeng.helloanimations.view.ViewAnimationDemoActivity;
 
 import butterknife.BindView;
@@ -44,7 +45,8 @@ public class MainActivity extends AppCompatActivity {
                 new Sample(R.string.view_animation_demo, ViewAnimationDemoActivity.class),
                 new Sample(R.string.card_flip_demo, CardFlipActivity.class),
                 new Sample(R.string.zoom_demo, ZoomActivity.class),
-                new Sample(R.string.simple_activity_transition_demo, SimpleActivityTransitionDemoActivity.class)
+                new Sample(R.string.simple_activity_transition_demo, SimpleActivityTransitionDemoActivity.class),
+                new Sample(R.string.simple_fragment_transition_demo, SimpleFragmentTransitionDemoActivity.class)
 
         };
         sampleListView.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,
---
+++ app/src/main/java/com/ddmeng/helloanimations/transition/fragment/SimpleFragmentTransitionDemoActivity.java
@@ -0,0 +1,75 @@
+package com.ddmeng.helloanimations.transition.fragment;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.ddmeng.helloanimations.R;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class SimpleFragmentTransitionDemoActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple_fragment_transition_demo);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.add_btn)
+    void addFragment() {
+        Fragment mTextFragmentOne = new FragmentV4();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .setCustomAnimations(
+                        R.anim.push_left_in,
+                        R.anim.push_left_out,
+                        R.anim.push_left_in,
+                        R.anim.push_left_out)
+                .add(R.id.container, mTextFragmentOne)
+                .addToBackStack(null)
+                .commit();
+    }
+
+    @OnClick(R.id.remove_btn)
+    void removeFragment() {
+        getSupportFragmentManager().popBackStack();
+    }
+
+    public static class FragmentV4 extends Fragment {
+
+        private static int sCount = 0;
+        private int mId = 0;
+        private final int[] mColors = new int[]{Color.RED, Color.GREEN,
+                Color.BLUE, Color.BLACK};
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            ++sCount;
+            mId = sCount;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+
+            TextView textView = new TextView(getContext());
+            textView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT));
+            textView.setText(String.format(getString(R.string.fragment_no), mId));
+            textView.setBackgroundColor(mColors[mId % mColors.length]);
+
+            return textView;
+        }
+    }
+}
